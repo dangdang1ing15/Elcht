5c93a557843192773200ca2885a29b37e7648476
dangdang1ing15 <shine_0512@naver.com>
2025-06-15 00:30:25 +0900

    Initial Commit

---
+++ Elcht/ElchtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElchtApp.swift
+//  Elcht
+//
+//  Created by 성현 on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ElchtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElchtTests/ElchtTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElchtTests.swift
+//  ElchtTests
+//
+//  Created by 성현 on 6/15/25.
+//
+
+import Testing
+@testable import Elcht
+
+struct ElchtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
